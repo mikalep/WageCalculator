42885b4d1f3472fab5084fbb918ce711e79c7fda
mikalep <itstenlayers@gmail.com>
2020-11-17 09:48:34 +0200

    Rename function to be more descriptive and add dynamic output of placeholder values based on selection

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function chooseTitle() {
+function onOptionChange() {
   const value = document.getElementById("selection").value;
   const title = document.getElementById("amount_box");
+  const text = document.getElementById("amount_input");
 
   if (value === "hourly") {
     title.innerHTML = "Please enter your monthly wage:";
+    text.placeholder = "Enter an amount, eg. 4000";
   } else {
     title.innerHTML = "Please enter your hourly wage:";
+    text.placeholder = "Enter an amount, eg. 30";
   }
 }
