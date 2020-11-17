cd53549d28c6ad0ff2f9e7e9f208513b6269c034
mikalep <itstenlayers@gmail.com>
2020-11-17 08:49:46 +0200

    Reformat the chooseTitle function by eliminating redundant code.

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 function chooseTitle() {
+  const value = document.getElementById("selection").value;
+  const title = document.getElementById("amount_box");
 
-    const value = document.getElementById("selection").value;
-    const title = document.getElementById("amount_box")
-
-    if (value === "hourly") {
-        document.getElementById("amount_box").innerHTML = "Please enter your salary:";
-    } else {
-        document.getElementById("amount_box").innerHTML = "Please enter your hourly wage:";
-    }
+  if (value === "hourly") {
+    title.innerHTML = "Please enter your salary:";
+  } else {
+    title.innerHTML = "Please enter your hourly wage:";
+  }
 }
